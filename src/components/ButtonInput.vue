89d68d28dd5dfbab9cd40c8f58d0eaa5ed3ab318
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2025-06-15 00:01:15 +0200

    Create button

---
+++ src/components/ButtonInput.vue
@@ -0,0 +1,25 @@
+<template>
+    <button class="btn-primary">
+        <slot />
+    </button>
+</template>
+
+<style lang="scss" scoped>
+.btn-primary {
+    background-color: var(--primary);
+    border: 2px solid black;
+    font-family: inherit;
+    font-size: 2rem;
+    font-weight: 600;
+    padding: .5rem 1rem;
+    box-shadow: 5px 5px 0 black;
+    margin-top: 2rem;
+    margin-bottom: 2rem;
+    cursor: pointer;
+    transition: all .2s;
+}
+.btn-primary:hover {
+    // transform: scale(1.05);
+    box-shadow: 8px 8px 0 black;
+}
+</style>
