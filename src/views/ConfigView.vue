e822777d7ef0ce7de5e99b2298b92e78b7edfd2b
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2025-06-16 16:02:48 +0200

    Add config screen

---
+++ src/views/ConfigView.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>Configuration</h1>
+
+    <label for="">Blinken server</label>
+    <input v-model="server" type="text">
+
+    <label for="">caspar channel</label>
+    <input v-model="ccg" type="text">
+</template>
+
+<script setup lang="ts">
+import { useLocalStorage } from '@vueuse/core';
+
+const server = useLocalStorage('blinken_server', '');
+const ccg = useLocalStorage('blinken_caspar_channel', '');
+
+</script>
