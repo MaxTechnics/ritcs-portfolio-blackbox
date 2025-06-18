ef3b44ba4a1fb781a17c61d7b2c844b609390f57
Maxim Coppieters <58440941+MaxTechnics@users.noreply.github.com>
2025-06-18 13:02:09 +0200

    Route rework

--- src/router/defs.ts
+++ src/router/defs.ts
@@ -10,7 +10,7 @@ const router = createRouter({
             // route level code-splitting
             // this generates a separate chunk (thing.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
-            component: () => import('@/views/HomeView.vue'),
+            component: () => import('@/views/StudentView.vue'),
         },
         {
             path: '/projects',
